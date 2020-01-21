eef24a14ab72217471b59aede3b75f3b5ec62bfd
aocattleya <aocattleya@yahoo.co.jp>
2020-01-22 05:37:10 +0900

    update: “status” テキストを交互に変更

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '◯');
 
     return (
       <div>
