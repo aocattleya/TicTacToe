d47c67a21e04e7fca8c2b5795adce7a19cf8966a
aocattleya <aocattleya@yahoo.co.jp>
2020-01-25 07:12:19 +0900

    update: stateの初期値, Game内のjumpToメソッドでstepNumberが更新されるように、値が偶数だった場合はxIsNextをtrueに設定

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
@@ -70,6 +71,13 @@ class Game extends React.Component {
         squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
