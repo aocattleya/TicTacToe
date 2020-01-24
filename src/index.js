a9196735a224a92860e14e35f38523fd8e1de5a4
aocattleya <aocattleya@yahoo.co.jp>
2020-01-25 06:59:43 +0900

    update: handleClickメソッドをBoardコンポーネントからGameコンポーネントに移動

--- src/index.js
+++ src/index.js
@@ -14,18 +14,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : '◯';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
@@ -67,6 +55,22 @@ class Game extends React.Component {
       }],
       xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : '◯';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
