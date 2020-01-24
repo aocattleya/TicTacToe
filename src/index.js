f08ecb330e4437374d6425eaca4a498a6bd22387
aocattleya <aocattleya@yahoo.co.jp>
2020-01-25 07:01:02 +0900

    remove: 不要なコードを削除

--- src/index.js
+++ src/index.js
@@ -115,7 +115,6 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    console.log([a, b, c]);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
